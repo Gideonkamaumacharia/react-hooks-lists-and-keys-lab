493ee8fa44c9eecebaa1de39126074256d5b8356
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-25 11:12:59 +0300

    last commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
+function NavBar(props) {
+  // const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */
+    <>
+      <a href="#home" key={props.home}>home</a>
+      <a href="#about" key={props.about}>about</a>
+      <a href="#projects" key={props.projects}>projects</a>
+    </>
+  }</nav>;
 }
 
 export default NavBar;
